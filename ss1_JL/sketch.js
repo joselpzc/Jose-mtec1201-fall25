3df56d96b4cbce36284d7661d95ee0bc7e7264df
joselpzc <joselpz4521@gmail.com>
2025-09-12 11:01:04 -0400

    first homework

---
+++ ss1_JL/sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+  createCanvas(1280, 720);
+
+  rectMode(CENTER);
+
+  background(20,103,20);
+ 
+
+  stroke(233,85,456,233);
+  fill(0,200,250,127);
+  rect(1005,230,20,100);
+  rect(805,230,20,100);
+  triangle(300,20,24,240,320,234);
+
+  stroke(23,34,456,23);
+  ellipse(890,350,400,500);
+  ellipse(800,380,40,120);
+  ellipse(870,380,40,120);
+  triangle(980,455,855,620,750,450);
+
+  
+}
